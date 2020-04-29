74f6ae652d4ece0563d7f7e2fa6d1067b3f1b7e6
kelly r maass <kmaass77627@gmail.com>
2020-04-29 22:59:22 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,4 @@
 class SongsController < ApplicationController
-  def index
-    @songs = Song.all
-  end
-
-  def show
-    @song = Song.find(params[:id])
-  end
 
   def new
     @song = Song.new
@@ -47,7 +40,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, genre_ids: [], notes_attributes: [:content])
   end
 end
-
